c64598047c81df7c404a7613be6b213ca4bbc84b
Bogdan Oshatinskiy <limassola@yandex.ru>
2023-01-25 00:08:00 +0300

    feat: реализовать функцию рендера и добавления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 let editButton = document.querySelector('.profile__button-edit');
 let addButton = document.querySelector('.profile__button-add')
 
@@ -32,8 +59,8 @@ function openPopup(popup) {
 editButton.addEventListener('click', ()=>openPopup(popupEdit));
 addButton.addEventListener('click', ()=>openPopup(popupAdd));
 
-function closePopup (currentPopup) {
-    currentPopup.classList.remove('popup_opened')
+function closePopup (popup) {
+    popup.classList.remove('popup_opened')
 };
 
 
@@ -60,7 +87,7 @@ function handleFormSubmit (evt) {
     profileContent.textContent = nameInput.value;
     jobContent.textContent = jobInput.value;
 
-    closePopup ();
+    closePopup (popupEdit);
 }
 
 formElement.addEventListener('submit', handleFormSubmit); 
@@ -78,3 +105,50 @@ likeButtons.forEach(item => {
         currentButton.classList.toggle('cards__button_active')
     })
 })
+
+// render cards
+
+
+const cards = document.querySelector('.cards');
+const titleInput = document.querySelector('.form__item_type_title');
+const linkInput = document.querySelector('.form__item_type_link');
+
+const createCard = (cardName, cardLink) => {
+  const template = `
+  <div class="cards__item">
+    <img class="cards__image">
+    <div class="cards__heading">
+        <h2 class="cards__title"></h2>
+        <button type="button" class="button cards__button"></button>
+    </div>
+  </div>
+  `;
+
+  const container = document.createElement('div');
+  container.innerHTML = template;
+  container.querySelector('.cards__title').textContent = cardName;
+  container.querySelector('.cards__image').setAttribute("src", cardLink);
+
+  return container.firstElementChild;
+}
+
+const renderCard = (cardName, cardLink) => {
+  cards.prepend(createCard(cardName, cardLink))
+}
+
+initialCards.forEach((item)=>{
+  cardName = item.name;
+  cardLink = item.link
+  renderCard(cardName, cardLink);
+})
+
+function addFormHandler(evt, item){
+  evt.preventDefault();
+  const cardName = titleInput.value
+  const cardLink = linkInput.value
+  renderCard(cardName,cardLink);
+  closePopup (popupAdd);
+}
+
+
+popupAdd.addEventListener('submit', addFormHandler);
